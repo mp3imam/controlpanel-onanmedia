0e4db5fc364dd02d73b901b507f5fb611e963fca
adiansafrida <adiansafrida@gmail.com>
2023-05-04 08:51:10 +0700

    file

--- app/Views/master/grid_config.php
+++ app/Views/master/grid_config.php
@@ -1,11 +1,12 @@
 <style type="text/css">
-	.datagrid-row-over td{
-		background:#D0E5F5;
-	}
-	.datagrid-row-selected td{
-		background:#FBEC88;
-		color:#000;
-	}
+.datagrid-row-over td {
+    background: #D0E5F5;
+}
+
+.datagrid-row-selected td {
+    background: #FBEC88;
+    color: #000;
+}
 </style>
 
 
@@ -16,7 +17,7 @@
 
         <div class="row mb-2">
             <div class="col-sm-12">
-                
+
                 <div class="card card-info">
                     <div class="card-header">
                         <h3 class="card-title">
@@ -25,32 +26,37 @@
                     </div>
                     <div class="card-body">
                         <div id="gridnya_{$mod}">
-                            
+
                             <div class="row mb-1">
                                 <div class="col-sm-12 mb-1">
-                                    
+
                                     <div class="row mb-2">
                                         <div class="col-sm-4">
-                                            <input class="form-control form-control-sm" type="text" placeholder="Kolom Pencarian Data">
+                                            <input class="form-control form-control-sm" type="text"
+                                                placeholder="Kolom Pencarian Data">
                                         </div>
                                         <div class="col-sm-2">
-                                            <a href="javascript:void(0);" class="btn btn-warning btn-sm" onClick="" >Cari</a>
+                                            <!-- <a href="javascript:void(0);" class="btn btn-warning btn-sm"
+                                                onClick="filterData()">Cari</a> -->
                                         </div>
                                         <div class="col-sm-6 text-right">
                                             {if $tombol_std|default:'' eq 'true'}
-                                                <a href="javascript:void(0);" class="btn btn-success btn-sm" onClick="genformMaster('add', '{$mod}', '{$acak}');" >Tambah</a>
-                                                &nbsp;
-                                                <a href="javascript:void(0);" class="btn btn-primary btn-sm" onClick="genformMaster('edit', '{$mod}', '{$acak}');" >Ubah</a>
-                                                &nbsp;
-                                                <a href="javascript:void(0);" class="btn btn-danger btn-sm" onClick="genformMaster('delete', '{$mod}', '{$acak}');" >Hapus</a>
+                                            <a href="javascript:void(0);" class="btn btn-success btn-sm"
+                                                onClick="genformMaster('add', '{$mod}', '{$acak}');">Tambah</a>
+                                            &nbsp;
+                                            <a href="javascript:void(0);" class="btn btn-primary btn-sm"
+                                                onClick="genformMaster('edit', '{$mod}', '{$acak}');">Ubah</a>
+                                            &nbsp;
+                                            <a href="javascript:void(0);" class="btn btn-danger btn-sm"
+                                                onClick="genformMaster('delete', '{$mod}', '{$acak}');">Hapus</a>
                                             {/if}
                                         </div>
                                     </div>
 
                                 </div>
                                 <div class="col-sm-12">
-                                    
-                                    <div class="row" style="margin-bottom:0px !important;">	
+
+                                    <div class="row" style="margin-bottom:0px !important;">
                                         <div class="col-lg-12" id="grid_parent_{$mod}">
                                             <div id='grid_{$mod}'></div>
                                         </div>
@@ -71,13 +77,22 @@
 </section>
 
 <script>
-$(function(){
+$(function() {
     $('#grid_parent_{$mod}').css({
         //'height': (getClientHeight()-165)
-        'height':( {$height_grid|default:'getClientHeight()-255'} )
+        'height': ({
+            $height_grid | default: 'getClientHeight()-255'
+        })
     });
-    heighttree = (getClientHeight()-255)+'px';
+    heighttree = (getClientHeight() - 255) + 'px';
 
-    genGridMaster('{$mod}','grid_{$mod}', '', '');
+    genGridMaster('{$mod}', 'grid_{$mod}', '', '');
+
+    var table = $('#grid_{$mod}').DataTable();
+
+    function filterData() {
+        var nama = $('input[name="nama"]').val();
+        table.column(1).search(nama).draw();
+    }
 });
 </script>
