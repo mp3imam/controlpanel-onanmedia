bc28c793a3356a8a6e0cb123b4ee7b83e3aa6583
Imam Tantowi <mp3imam@gmail.com>
2023-12-12 23:37:57 +0700

    JasaSeeder

--- database/seeders/JasaSeeder.php
+++ database/seeders/JasaSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\JasaModel;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class JasaSeeder extends Seeder
 {
@@ -13,8 +13,33 @@ class JasaSeeder extends Seeder
      */
     public function run(): void
     {
-        $path = public_path('sql/_Jasa__202312121641.sql');
-        $sql = file_get_contents($path);
-        DB::unprepared($sql);
+        JasaModel::insert([
+            ['id'=>'clm5zx4ig000cjneydvysqrsh','nama'=>'Jasa landing page','msSubkategoriId'=>'4','msKategoriId'=>'1','tags'=>'{web,figma}','impresi'=>'0','klik'=>'0','userId'=>'clm5ykvva002sjnpt622cixhk','deskripsi'=>'ryhrtytytytut','cover'=>'https://dev-onanmedia.s3.ap-southeast-1.amazonaws.com/5bfb0dff2956c770381552f365676df7.jpg','slug'=>'jasa-landing-page','hargaTermahal'=>'200000','hargaTermurah'=>'200000','msStatusJasaId'=>'1','isPengambilan'=>'0','isPengiriman'=>'0'],
+            ['id'=>'clm60q1gr002yjney4jrdwxch','nama'=>'Mobile Design','msSubkategoriId'=>'4','msKategoriId'=>'1','tags'=>'{mobile,android,ios}','impresi'=>'0','klik'=>'0','userId'=>'clm5ykvva002sjnpt622cixhk','deskripsi'=>'Pembuatan mobile desain ','cover'=>'https://dev-onanmedia.s3.ap-southeast-1.amazonaws.com/F%26O%20Fabforgottennobility.jpg','slug'=>'mobile-design','hargaTermahal'=>'500000','hargaTermurah'=>'500000','msStatusJasaId'=>'1','isPengambilan'=>'0','isPengiriman'=>'0'],
+            ['id'=>'clm8utogl006ljneyzn86g1u8','nama'=>'Design logo','msSubkategoriId'=>'1','msKategoriId'=>'1','tags'=>'{logo,branding}','impresi'=>'0','klik'=>'0','userId'=>'clm5ykvva002sjnpt622cixhk','deskripsi'=>'jasa logo desain murah','cover'=>'https://dev-onanmedia.s3.ap-southeast-1.amazonaws.com/wp4347426-kingdom-manga-wallpapers.jpg','slug'=>'design-logo','hargaTermahal'=>'50000','hargaTermurah'=>'50000','msStatusJasaId'=>'1','isPengambilan'=>'0','isPengiriman'=>'0'],
+            ['id'=>'cloxsfx55002wjnhf5q6vjqov','nama'=>'Desain Katalog','msSubkategoriId'=>'8','msKategoriId'=>'1','tags'=>'{desain,desainkatalog}','impresi'=>'0','klik'=>'0','userId'=>'cloxry7b50024jnhfw9nel54b','deskripsi'=>'Apakah Anda membutuhkan Desain Katalog | Katalog Produk | Lembar Jual | Buku Pencarian | Brosur | Profil Perusahaan | Desain Buku | Usulan | Majalah | Presentasi dan proyek Grafis apa pun dengan menggunakan Perangkat Lunak Adobe.
+            Saya harap ini adalah tempat yang tepat untuk Anda.
+
+            Pertunjukan ini dirancang khusus untuk katalog produk atau perangkat pemasaran terkait produk juga yang sesuai dengan anggaran Anda. Kami akan mempertahankan Tren Grafis terkini dan standar Industri yang Unik untuk merancang Katalog Produk Anda.','cover'=>'https://dev-onanmedia.s3.ap-southeast-1.amazonaws.com/catalog%20%282%29.jpg','slug'=>'desain-katalog','hargaTermahal'=>'2000000','hargaTermurah'=>'2000000','msStatusJasaId'=>'1','isPengambilan'=>'0','isPengiriman'=>'0'],
+            ['id'=>'clmedjwsx000ejn79iigvk3dv','nama'=>'Kartu Bisnis','msSubkategoriId'=>'8','msKategoriId'=>'1','tags'=>'{desain,desaincard,card}','impresi'=>'0','klik'=>'0','userId'=>'clm8u0jhj006cjneyfys1ju01','deskripsi'=>'Dengan bantuan saya, Anda akan dapat membedakan bisnis Anda dari pesaing dan memposisikan merek Anda dengan tepat. Saya akan membantu Anda menonjol di mata pelanggan potensial Anda dengan kartu nama dan alat tulis mewah yang indah. Saya akan mendesain tata letak yang unik dari awal (saya tidak menggunakan template). File yang akan Anda terima memiliki penggunaan komersial penuh!
+            Segera setelah saya menerima konten, saya dapat mulai mengerjakan tata letak Anda.
+            Tolong berikan saya konten teks dan semua data bisnis yang diperlukan.
+            Penting: Saya tidak akan memeriksa ejaan/tata bahasa konten Anda.
+
+            Silakan baca FAQ.
+            Jika Anda memiliki pertanyaan, jangan ragu untuk menghubungi saya. Saya senang bertemu klien saya di Zoom / Skype. :)','cover'=>'https://dev-onanmedia.s3.ap-southeast-1.amazonaws.com/card.jpg','slug'=>'kartu-bisnis','hargaTermahal'=>'125000000','hargaTermurah'=>'5000000','msStatusJasaId'=>'1','isPengambilan'=>'0','isPengiriman'=>'0'],
+            ['id'=>'cloclhmar0001jnfgvzjb0aq9','nama'=>'Website dengan react','msSubkategoriId'=>'12','msKategoriId'=>'2','tags'=>'{js}','impresi'=>'0','klik'=>'0','userId'=>'clocklmu10031jn1hkffhbppb','deskripsi'=>'Web murah dan cepat','cover'=>'https://dev-onanmedia.s3.ap-southeast-1.amazonaws.com/onanmedia-05-05%20%281%29.png','slug'=>'website-dengan-react','hargaTermahal'=>'400000','hargaTermurah'=>'400000','msStatusJasaId'=>'1','isPengambilan'=>'0','isPengiriman'=>'0'],
+            ['id'=>'clmeient9001cjn792l7vhe1m','nama'=>'Optimasi Halaman SEO','msSubkategoriId'=>'25','msKategoriId'=>'5','tags'=>'{seo,optimasiseo,halamanseo,laptop}','impresi'=>'0','klik'=>'0','userId'=>'clm8u0jhj006cjneyfys1ju01','deskripsi'=>'Mengikuti faktor peringkat Google bisa menjadi tugas yang menantang, mengingat evolusi konstan dari algoritma mereka. Namun, Anda dapat yakin mengetahui bahwa saya akan menangani semua elemen SEO on-page yang diperlukan untuk memastikan kepatuhan dan mengoptimalkan peringkat situs web Anda.
+
+            ','cover'=>'https://dev-onanmedia.s3.ap-southeast-1.amazonaws.com/seo.jpg','slug'=>'optimasi-halaman-seo','hargaTermahal'=>'1700000','hargaTermurah'=>'750000','msStatusJasaId'=>'1','isPengambilan'=>'1','isPengiriman'=>'0'],
+            ['id'=>'clpc0jot8018hjnhf14h806cv','nama'=>'Mengubah pdf ke png','msSubkategoriId'=>'7','msKategoriId'=>'1','tags'=>'{}','impresi'=>'0','klik'=>'0','userId'=>'cloclkjhm0007jnfggu1zrn11','deskripsi'=>'Mengubah File PDF anda menjadi PNG','cover'=>'https://dev-onanmedia.s3.ap-southeast-1.amazonaws.com/icon.png','slug'=>'mengubah-pdf-ke-png','hargaTermahal'=>'20000','hargaTermurah'=>'10000','msStatusJasaId'=>'1','isPengambilan'=>'0','isPengiriman'=>'0'],
+            ['id'=>'clmeekb03000zjn79dinmpbc4','nama'=>'Merancang Katalog Professional','msSubkategoriId'=>'7','msKategoriId'=>'1','tags'=>'{katalog,merancangkatalog,jam}','impresi'=>'0','klik'=>'0','userId'=>'clm8u0jhj006cjneyfys1ju01','deskripsi'=>'Sebagai perancang katalog berpengalaman, saya membuat katalog yang menakjubkan secara visual dan informatif yang akan menarik perhatian pelanggan Anda dan secara efektif menyampaikan pesan merek Anda.
+
+            Dengan pertunjukan saya, Anda dapat mengharapkan katalog yang dirancang khusus yang disesuaikan dengan kebutuhan dan persyaratan spesifik Anda. Saya akan menggunakan tata letak yang menarik dan unik yang akan membuat produk Anda menonjol, dan menggabungkan gambar dan grafik berkualitas tinggi yang akan membuat katalog Anda menarik secara visual.','cover'=>'https://dev-onanmedia.s3.ap-southeast-1.amazonaws.com/catalog.jpg','slug'=>'merancang-katalog-professional','hargaTermahal'=>'750000','hargaTermurah'=>'250000','msStatusJasaId'=>'1','isPengambilan'=>'1','isPengiriman'=>'0'],
+            ['id'=>'clp7rjie700v2jnhfduch9aqa','nama'=>'Jasa Desain Komik Murah dan Cepat','msSubkategoriId'=>'10','msKategoriId'=>'1','tags'=>'{komik,desain,ilustrasi,seni}','impresi'=>'0','klik'=>'0','userId'=>'clocklmu10031jn1hkffhbppb','deskripsi'=>'Jasa pembuatan komik murah hanya dengan AI
+
+            link portfolio: https://www.bilibili.tv/id','cover'=>'https://dev-onanmedia.s3.ap-southeast-1.amazonaws.com/peakpx%20%281%29.jpg','slug'=>'jasa-desain-komik-murah-dan-cepat','hargaTermahal'=>'1000000','hargaTermurah'=>'300000','msStatusJasaId'=>'1','isPengambilan'=>'0','isPengiriman'=>'0'],
+            ['id'=>'clp69qngh00tajnhftd09wlcr','nama'=>'Jasa sedang di verifikasi','msSubkategoriId'=>'14','msKategoriId'=>'2','tags'=>'{qa,qualityassurance}','impresi'=>'0','klik'=>'0','userId'=>'cloxry7b50024jnhfw9nel54b','deskripsi'=>'It would be my pleasure to help you make your product the best it can be. I will assist you in making sure that your website/software/application/mobile application runs smoothly as well as meets the needs of your customers. I will spend time analyzing and testing thoroughly through logs, screenshots, videos, etc. I want to make sure you release without any bugs or errors as well as maximizing your users experience.','cover'=>'https://dev-onanmedia.s3.ap-southeast-1.amazonaws.com/QAimage.jpg','slug'=>'jasa-sedang-di-verifikasi','hargaTermahal'=>'2000000','hargaTermurah'=>'2000000','msStatusJasaId'=>'1','isPengambilan'=>'0','isPengiriman'=>'0'],
+                    ]);
     }
 }
