f54ab2691f35a03e45e6f7e725996f3adbd83f9e
imamTampan <mp3imam@gmail.com>
2023-12-13 13:39:21 +0700

    css ngrok

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&  $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
         //
-        Schema::defaultStringLength(191);
-        Paginator::useBootstrap();
+        // Schema::defaultStringLength(191);
+        // Paginator::useBootstrap();
 
     }
 }
