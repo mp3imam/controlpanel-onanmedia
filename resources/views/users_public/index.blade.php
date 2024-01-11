2163760714bf5f2fc0fc4dc6051d1bfaf02da412
triwahyunugroho11 <triwahyunugroho11@gmail.com>
2024-01-11 14:08:06 +0700

    commit perbaikan style

--- resources/views/users_public/index.blade.php
+++ resources/views/users_public/index.blade.php
@@ -10,53 +10,72 @@
     <div class="col-lg-12">
         <div class="card">
             <div class="card-body">
-                <div id="customerList">
+                <!-- <div id="customerList">
                     <div class="col-sm-auto mb-3">
-                    {{-- <button type="button" class="btn btn-success" onclick="modal_crud('Tambah')" data-bs-toggle="modal" data-bs-target="#exampleModalgrid">
-                        Tambah
-                    </button> --}}
-                    <a type="button" class="btn btn-primary btn-label btn-pdf">
-                            <div class="d-flex">
-                                <div class="flex-shrink-0">
-                                    <i class="bx bxs-file-pdf label-icon align-middle fs-16 me-2"></i>
-                                </div>
-                                <div class="flex-grow-1 btn-pdf-loading" hidden>
-                                    Loading...
-                                </div>
-                                <div class="flex-grow-1 btn-pdf-no-loading">
-                                    PDF
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+
+                    </div> 
                     <div class="row g-4">
                         <div class="row mt-4">
                             <div class="col-xxl-4 col-md-6 p-3">
-                                <label>Filter UserName</label>
-                                <input id='username_id' name="username_id" />
+                                <label>Filter UserName</label> <input id='username_id' name="username_id" />
                             </div>
                         </div>
                     </div>
                     <div class="card">
                         <div class="card-body">
-                            <table id="dataTable" class="table table-striped table-bordered table-sm " cellspacing="0" width="100%">
-                                <thead>
-                                    <tr>
-                                        <th width="5px">Nomor</th>
-                                        <th width="100px">Nama Lengkap</th>
-                                        <th width="30px">Nomor Hanphone</th>
-                                        <th width="30px">Username</th>
-                                        <th width="30px">Verifikasi Email</th>
-                                        <th width="30px">Verifikasi No.Hp</th>
-                                        <th width="30px">Terdaftar Seller</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                </tbody>
-                            </table>
+                            
                         </div>
                     </div>
+
+                     <th width="5px">Nomor</th>
+                            <th width="100px">Nama Lengkap</th>
+                            <th width="30px">Nomor Hanphone</th>
+                            <th width="30px">Username</th>
+                            <th width="30px">Verifikasi Email</th>
+                            <th width="30px">Verifikasi No.Hp</th>
+                            <th width="30px">Terdaftar Seller</th>
+                </div> -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+
+                                <div class="row">
+                                    <div class="col-md-2">
+                                        <div>
+                                            <label for="basiInput" class="form-label">Status User</label>
+                                            <select id="status_user" class="form-select">
+                                                <option value="">-- Pilih --</option>
+                                                <option value="0">Pembeli</option>
+                                                <option value="1">Penjual</option>
+                                                <option value="2">Pengajuan Seller</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <table id="dataTable" class="table table-striped table-bordered table-sm " cellspacing="0" width="100%">
+                            <thead>
+                                <tr>
+                                    <th >No</th>
+                                    <th >Nama Lengkap</th>
+                                    <th >No. Handphone</th>
+                                    <th >Username</th>
+                                    <th >Verif. Email</th>
+                                    <th >Verif. No.Hp</th>
+                                    <th >Terdaftar Seller</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
+                
                 <div class="modal fade" id="exampleModalgrid" tabindex="-1" aria-labelledby="exampleModalgridLabel" data-bs-backdrop="static" aria-modal="true" role="dialog" style="display: none;">
                     <div class="modal-dialog modal-xl" id="modal_content">
                     </div>
@@ -65,7 +84,16 @@
         </div>
     </div>
 </div>
-
+<style>
+    #dataTable thead th {
+        background-color: #405189;
+        color: #ffffff;
+        text-align: center;
+        text-transform: uppercase;
+        height:40px;
+        vertical-align: middle;
+    }
+</style>
 @endsection
 
 @section('script')
@@ -75,49 +103,54 @@
     var img_pengajuan = `<img src="{{ URL::asset('assets/images/logo/pengajuan.png') }}" alt=""height="30px">`
     $(function () {
         var table = $('#dataTable').DataTable({
-            dom: 'lrtip',
+            //dom: 'lrtip',
+            //dom: '<"top"f>rt<"bottom"lip><"clear">',
             processing: true,
             serverSide: true,
+            ordering: false,
+            "deferRender": true,
             ajax: {
                 url: "{{ route('users_public.create') }}",
                 data: function (d) {
-                    d.username_id = $('#username_id').val()
-                    d.roles_id = $('#roles_id').val()
+                    //d.username_id = $('#username_id').val()
+                    //d.roles_id = $('#roles_id').val()
+
+                    d.status_user = $('#status_user').val();
                 }
             },
             columns: [{
                     data: "id",
                     sortable: false,
+                    width: '5%',
+                    className: 'dt-center',
                     render: function (data, type, row, meta) {
                         return meta.row + meta.settings._iDisplayStart + 1;
                     }
                 },{
                     data: 'name',
-                    name: 'Nama Lengkap',
+                    width : "30%",
                     render: function (data, type, row) {
                         return `<button class="btn btn-ghost-primary waves-effect waves-light text-right btn-sm" type="button" target="_blank" onclick="modal_crud('Edit', '`+row.id+`', '`+row.name+`', '`+row.email+`', '`+row.phone+`', '`+row.isEmailVerified+`', '`+row.isPhoneVerified+`', '`+row.sellerStatus+`')" data-bs-toggle="modal" data-bs-target="#exampleModalgrid">`+data.trim()+`</button>`;
                     }
                 },{
                     data: 'email',
-                    name: 'Email'
                 },{
                     data: 'phone',
-                    name: 'No Handphone'
                 },{
                     data: 'isEmailVerified',
-                    name: 'Verifikasi Email',
+                    className: 'dt-center',
                     render: function (data) {
                         return data ? img_ok : img_not_ok;
                     }
                 },{
                     data: 'isPhoneVerified',
-                    name: 'Verifikasi No Handphone',
+                    className: 'dt-center',
                     render: function (data) {
                         return data ? img_ok : img_not_ok;
                     }
                 },{
                     data: 'sellerStatus',
-                    name: 'Terdaftar Seller',
+                    className: 'dt-center',
                     render: function (data) {
                         return data == 2 ? img_pengajuan : data == 1 ? img_ok : img_not_ok;
                     }
@@ -125,13 +158,10 @@
             ]
         });
 
-        $('#username_id').keyup(function () {
+        $('#status_user').on('change', function () {
             table.draw();
         });
 
-        $('#roles_id').change(function () {
-            table.draw();
-        });
     });
 
     function modal_crud(data, id, name, email, phone, isEmailVerified, isPhoneVerified, sellerStatus){
@@ -500,3 +530,23 @@
     });
 </script>
 @endsection
+
+
+{{-- 
+<button type="button" class="btn btn-success" onclick="modal_crud('Tambah')" data-bs-toggle="modal" data-bs-target="#exampleModalgrid">
+    Tambah
+</button> 
+<a type="button" class="btn btn-primary btn-label btn-pdf">
+    <div class="d-flex">
+        <div class="flex-shrink-0">
+            <i class="bx bxs-file-pdf label-icon align-middle fs-16 me-2"></i>
+        </div>
+        <div class="flex-grow-1 btn-pdf-loading" hidden>
+            Loading...
+        </div>
+        <div class="flex-grow-1 btn-pdf-no-loading">
+            PDF
+        </div>
+    </div>
+</a>
+--}}
