58309f323c1ff523a6984d227e883f0ba0b7fca0
Imam OnanMedia <mp3imam@gmail.com>
2024-05-13 10:26:26 +0700

    tampilan not found onanmedia

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,36 +1,152 @@
-@extends('layouts.master-without-nav')
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg"
+    data-sidebar-image="none" data-preloader="disable" data-bs-theme="light" data-layout-width="fluid"
+    data-layout-position="fixed" data-layout-style="default">
 
-@section('title')
-@lang('translation.Error_404')
-@endsection
+<head>
 
-@section('body')
-<body>
-@endsection
-@section('content')
-    <div class="auth-page-wrapper py-5 d-flex justify-content-center align-items-center min-vh-100">
+    <meta charset="utf-8">
+    <title>404 Error Basic | Velzon - Admin &amp; Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin &amp; Dashboard Template" name="description">
+    <meta content="Themesbrand" name="author">
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
 
-    <!-- auth-page content -->
-    <div class="auth-page-content overflow-hidden p-0">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xl-7 col-lg-8">
-                    <div class="text-center">
-                        <img src="{{ asset('assets/images/error400-cover.png') }}" alt="error img" class="img-fluid">
-                        <div class="mt-3">
-                            <h3 class="text-uppercase">Sorry, Page not Found 😭</h3>
-                            <p class="text-muted mb-4">The page you are looking for not available!</p>
-                            <a href="/" class="btn btn-primary"><i class="mdi mdi-home me-1"></i>Back to home</a>
+    <!-- Layout config Js -->
+    <script src="assets/js/layout.js"></script>
+    <!-- Bootstrap Css -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <!-- Icons Css -->
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css">
+    <!-- App Css-->
+    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css">
+    <!-- custom Css-->
+    <link href="assets/css/custom.min.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body inmaintabuse="1">
+
+    <div class="auth-page-wrapper pt-5">
+        <!-- auth page bg -->
+        <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
+            <div class="bg-overlay"></div>
+
+            <div class="shape">
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 1440 120">
+                    <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
+                </svg>
+            </div>
+            <canvas class="particles-js-canvas-el" width="4066" height="760"
+                style="width: 100%; height: 100%;"></canvas>
+        </div>
+
+        <!-- auth page content -->
+        <div class="auth-page-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center pt-4">
+                            <div class="">
+                                <img src="assets/images/error.svg" alt=""
+                                    class="error-basic-img move-animation">
+                            </div>
+                            <div class="mt-n4">
+                                <h1 class="display-1 fw-medium">404</h1>
+                                <h3 class="text-uppercase">Sorry, Page not Found 😭</h3>
+                                <p class="text-muted mb-4">The page you are looking for not available!</p>
+                                <a href="index.html" class="btn btn-success"><i class="mdi mdi-home me-1"></i>Back to
+                                    home</a>
+                            </div>
                         </div>
                     </div>
-                </div><!-- end col -->
-            </div>
-            <!-- end row -->
-        </div>
-        <!-- end container -->
-    </div>
-    <!-- end auth-page content -->
-    </div>
-        <!-- end auth-page-wrapper -->
+                </div>
+                <!-- end row -->
 
-@endsection
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end auth page content -->
+
+        <!-- footer -->
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center">
+                            <p class="mb-0 text-muted">©
+                                <script>
+                                    document.write(new Date().getFullYear())
+                                </script> Onanmedia
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- end Footer -->
+
+    </div>
+    <!-- end auth-page-wrapper -->
+
+    <!-- JAVASCRIPT -->
+    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/libs/simplebar/simplebar.min.js"></script>
+    <script src="assets/libs/node-waves/waves.min.js"></script>
+    <script src="assets/libs/feather-icons/feather.min.js"></script>
+    <script src="assets/js/pages/plugins/lord-icon-2.1.0.js"></script>
+    <script src="assets/js/plugins.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+    <script type="text/javascript" src="assets/libs/choices.js/public/assets/scripts/choices.min.js"></script>
+    <script type="text/javascript" src="assets/libs/flatpickr/flatpickr.min.js"></script>
+
+
+    <!-- particles js -->
+    <script src="assets/libs/particles.js/particles.js"></script>
+    <!-- particles app js -->
+    <script src="assets/js/pages/particles.app.js"></script>
+
+
+
+    <script src="chrome-extension://igkkmokkmlbkkgdnkkancbonkbbmkioc/sm.bundle.js" data-pname="recorder-screenshot-v3"
+        data-asset-path="https://apv3.s3.ap-northeast-2.amazonaws.com"></script>
+    <div style="display: none" class="ubey-RecordingScreen-count-down ubey-RecordingScreen-count-down-container">
+        <style>
+            .ubey-RecordingScreen-count-down-container {
+                position: fixed;
+                height: 100vh;
+                width: 100vw;
+                top: 0;
+                left: 0;
+                z-index: 9999999999999;
+                background-color: rgba(0, 0, 0, 0.2);
+            }
+
+            .ubey-RecordingScreen-count-down-content {
+                position: absolute;
+                display: flex;
+                top: 50%;
+                left: 50%;
+                justify-content: center;
+                align-items: center;
+                color: white;
+                height: 15em;
+                width: 15em;
+                transform: translate(-50%, -100%);
+                background-color: rgba(0, 0, 0, 0.6);
+                border-radius: 50%;
+            }
+
+            #ubey-RecordingScreen-count-count {
+                font-size: 14em;
+                transform: translateY(-2%);
+            }
+        </style>
+        <div class="ubey-RecordingScreen-count-down-content">
+            <span id="ubey-RecordingScreen-count-count"></span>
+        </div>
+    </div>
+</body>
+
+</html>
